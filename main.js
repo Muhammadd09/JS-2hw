47048fbdc729f158706d4ea36768ce24849d298c
Muhammad <muzammadshuhratov@gmail.com>
2023-01-12 13:21:27 +0500

    js-2hw

---
+++ main.js
@@ -0,0 +1,64 @@
+
+
+
+var num = prompt('Misolni yeching: 7 + 7 = ? ');
+
+if (num === '14') {
+    alert('Javobingiz togri ')     
+    console.log('1) Sizning javobingiz togri ');
+} else {
+    alert('Javobingiz notogri ')
+    console.log('1) Siz notogri javob berdingiz ');
+}
+
+
+   
+var num = prompt('Misolni yeching: 7 * 7 = ? ');
+
+if (num === '49') {
+    alert('Javobingiz togri ')
+    console.log('2) Sizning javobingiz togri ');
+} else {
+    alert('Javobingiz notogri ')
+    console.log('2) Siz notogri javob berdingiz ');
+}
+
+
+
+
+var num = prompt('Misolni yeching: 16 - 6 = ? ');
+
+if (num === '10') {
+    alert('Javobingiz togri ')
+    console.log('3) Sizning javobingiz togri ');
+} else {
+    alert('Javobingiz notogri ')
+    console.log('3) Siz notogri javob berdingiz ');
+}
+
+
+
+var num = prompt('Misolni yeching: 49 / 7 = ? ');
+
+if (num === '7') {
+    alert('Javobingiz togri ')
+    console.log('4) Sizning javobingiz togri ');
+} else {
+    alert('Javobingiz notogri ')
+    console.log('4) Siz notogri javob berdingiz ');
+}
+
+
+
+
+var num = prompt('Misolni yeching: 11 % 2 = ? ');
+
+if (num === '1') {
+    alert('Javobingiz togri ')
+    console.log('5) Sizning javobingiz togri ');
+} else {
+    alert('Javobingiz notogri ')
+    console.log('6) Siz notogri javob berdingiz ');
+}
+
+
